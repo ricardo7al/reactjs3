ba70f372bbf5a68557133bbc8f83b2398e7ffe13
unknown <ricardo7almail.com>
2020-04-02 17:24:30 -0300

    Updates

---
+++ precache-manifest.c3e581fc6889d0d13e52f3e670b361c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b8e5ca14440c592620a7c14b1e7981",
+    "url": "/apptest/index.html"
+  },
+  {
+    "revision": "2ec6e64dbe12da0b04f3",
+    "url": "/apptest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47d123f8a1368b83fa44",
+    "url": "/apptest/static/js/2.218a5fc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apptest/static/js/2.218a5fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec6e64dbe12da0b04f3",
+    "url": "/apptest/static/js/main.95480662.chunk.js"
+  },
+  {
+    "revision": "f7b1fb616f232a2967da",
+    "url": "/apptest/static/js/runtime-main.1a6cb022.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/apptest/static/media/logo.5d5d9eef.svg"
+  }
+]);
